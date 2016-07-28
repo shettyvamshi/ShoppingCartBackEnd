bc8c93d50d14013bfff8153931cc258b442d1505
vamshi shetty <shetty.dob@gmail.com>
2016-07-28 20:51:40 +0530

    Add files via upload

---
+++ ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/model/User.java
@@ -0,0 +1,45 @@
+package com.niit.shoppingcart.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table
+@Component
+public class User {
+	
+	@Id
+	private String id;
+	private String name;
+	private String password;
+	private String mobile;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+
+}
